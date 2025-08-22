b5f8b4fba413219303116b3a85a317aa2ab6fb14
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2025-08-22 13:37:21 +0800

    Add skeleton code for API

---
+++ weather-egg-api/src/main/java/com/kiyan/weathereggapi/WeatherEggApiApplication.java
@@ -0,0 +1,15 @@
+package com.kiyan.weathereggapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherEggApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherEggApiApplication.class, args);
+    }
+
+
+
+}
---
+++ weather-egg-api/src/test/java/com/kiyan/weathereggapi/WeatherEggApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiyan.weathereggapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherEggApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

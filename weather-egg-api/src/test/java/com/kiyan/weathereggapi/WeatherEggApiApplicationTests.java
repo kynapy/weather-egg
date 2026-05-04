d59559dbe1f145e305681a7df9ebcea94b6edf77
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2026-05-04 13:41:04 +0800

    Add H2 dependency and use H2 for tests

--- weather-egg-api/src/test/java/com/kiyan/weathereggapi/WeatherEggApiApplicationTests.java
+++ weather-egg-api/src/test/java/com/kiyan/weathereggapi/WeatherEggApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.kiyan.weathereggapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WeatherEggApiApplicationTests {
 
     @Test

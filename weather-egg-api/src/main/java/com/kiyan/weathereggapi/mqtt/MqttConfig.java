edd7831e018136abb13e0bcc61b92d0f8e8f4257
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2026-05-05 20:20:21 +0800

    Exclude mqttConfiguration from test

--- weather-egg-api/src/main/java/com/kiyan/weathereggapi/mqtt/MqttConfig.java
+++ weather-egg-api/src/main/java/com/kiyan/weathereggapi/mqtt/MqttConfig.java
@@ -3,6 +3,7 @@ package com.kiyan.weathereggapi.mqtt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.core.MessageProducer;
@@ -11,6 +12,7 @@ import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.MessageChannel;
 
 @Configuration
+@Profile("!test")
 @EnableIntegration
 public class MqttConfig {
     @Value("${MQTT_BROKER_URL}")
